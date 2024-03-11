7268419f38ce8e82caf9f626bd9f637b023b235d
J0sa3210 <joeri.winckelmans@outlook.be>
2024-03-11 10:24:26 +0100

    Auto stash before checking out "origin/mapsaver"

---
+++ src/main/java/com/example/ds_project/database/I_NamingserverDB.java
@@ -0,0 +1,20 @@
+package com.example.ds_project.database;
+
+import java.net.Inet4Address;
+import java.util.HashMap;
+
+public interface I_NamingserverDB {
+    //String filePath;
+
+    // Load the map from file
+    void load();
+
+    // Save the map to file
+    void save();
+
+    // Get address from hash
+    Inet4Address get(Integer hash);
+
+    // Put address with key hash
+    void put(Integer hash, Inet4Address ip);
+}
