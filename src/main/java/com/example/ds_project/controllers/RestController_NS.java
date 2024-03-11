e2c4d38265e2c15facf47a71621fe716a3c6951f
ZioltZe <eliott.vandenhoudt@gmail.com>
2024-03-11 10:11:48 +0100

    RestController : Update

--- src/main/java/com/example/ds_project/controllers/RestController_NS.java
+++ src/main/java/com/example/ds_project/controllers/RestController_NS.java
@@ -1,12 +1,11 @@
 package com.example.ds_project.controllers;
 
+import com.example.ds_project.NamingServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @RestController
 public class RestController_NS
@@ -17,16 +16,9 @@ public class RestController_NS
      * @return the IP-address of the file location
      */
     @GetMapping("/project/searchFile")
-    public Inet4Address SearchFile(@RequestParam String fileName) throws UnknownHostException
+    public Inet4Address SearchFile(@RequestParam String fileName)
     {
-        Inet4Address ip = (Inet4Address) InetAddress.getByName("192.168.1.60");
-
-        // if file found
-        if(false)
-            return ip;
-        // if file not found
-        else
-            return null;
-
+        NamingServer ns = new NamingServer(); // TEMP
+        return ns.getLocationIP(fileName);
     }
 }
