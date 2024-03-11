f07b3a46168bbe61fa17cae3c385f54cd6609da0
ZioltZe <eliott.vandenhoudt@gmail.com>
2024-03-11 09:42:05 +0100

    RestController : Update

--- src/main/java/com/example/ds_project/controllers/RestController_NS.java
+++ src/main/java/com/example/ds_project/controllers/RestController_NS.java
@@ -1,6 +1,7 @@
 package com.example.ds_project.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.Inet4Address;
@@ -10,11 +11,12 @@ public class RestController_NS
 {
     /**
      * Asks the naming server for the location of a file
+     * @param fileName the name of the file
      * @return the IP-address of the file location
      */
-    @GetMapping("/")
-    public Inet4Address SearchFile()
+    @GetMapping("/GetFileLocation")
+    public Inet4Address SearchFile(@RequestParam String fileName)
     {
-
+        return null;
     }
 }
