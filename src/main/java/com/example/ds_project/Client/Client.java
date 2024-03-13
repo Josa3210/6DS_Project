7509355fefc012817a85573ddb26391c9103578b
Thomas Van Ostade <thomas.vanostade@student.uantwerpen.be>
2024-03-13 17:21:52 +0100

    Worked on client class.

--- src/main/java/com/example/ds_project/Client/Client.java
+++ src/main/java/com/example/ds_project/Client/Client.java
@@ -1,4 +1,27 @@
 package com.example.ds_project.Client;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
 public class Client {
+
+    public void loadfile() {
+
+        String imagePath = "kittler.jpg"; // Specify the path to your image file
+
+        try {
+            File file = new File(imagePath);
+            BufferedImage image = ImageIO.read(file);
+
+            // Do something with the image, e.g., display it or process it further
+            System.out.println("Image width: " + image.getWidth());
+            System.out.println("Image height: " + image.getHeight());
+        } catch (IOException e) {
+            System.err.println("Error reading the image: " + e.getMessage());
+        }
+    }
+
 }
