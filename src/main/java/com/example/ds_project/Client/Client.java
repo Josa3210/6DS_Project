2b8e47b5014e03846b3a90659e907559ccf0c940
J0sa3210 <joeri.winckelmans@outlook.be>
2024-04-02 15:15:29 +0200

    Created an interface for the client

--- src/main/java/com/example/ds_project/Client/Client.java
+++ src/main/java/com/example/ds_project/Client/Client.java
@@ -4,9 +4,56 @@ import com.example.ds_project.namingServer.NamingServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.Inet4Address;
+
 @SpringBootApplication
-public class Client {
+public class Client implements I_Client{
     public static void main(String[] args) {
         SpringApplication.run(NamingServer.class, args);
+    }
+
+    @Override
+    public int computeHash(String s) {
+        return 0;
+    }
+
+    @Override
+    public void habari() {
+
+    }
+
+    @Override
+    public void karibu() {
+
+    }
+
+    @Override
+    public void shutDown() {
+
+    }
+
+    @Override
+    public void sendLinkID(Inet4Address nodeIP, int startID, int otherID) {
+
+    }
+
+    @Override
+    public void receiveLinkID() {
+
+    }
+
+    @Override
+    public void removeFromNS(Inet4Address nsIP, Inet4Address nodeIP) {
+
+    }
+
+    @Override
+    public void ping(Inet4Address hostIP) {
+
+    }
+
+    @Override
+    public void removeFromNetwork(Inet4Address hostIP) {
+
     }
 }
