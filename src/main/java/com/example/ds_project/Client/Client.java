7d257d1d14c308b10b4e07b5395b553c171be0fc
J0sa3210 <joeri.winckelmans@outlook.be>
2024-04-02 12:10:07 +0200

    made client a springboot application

--- src/main/java/com/example/ds_project/Client/Client.java
+++ src/main/java/com/example/ds_project/Client/Client.java
@@ -1,4 +1,12 @@
 package com.example.ds_project.Client;
 
+import com.example.ds_project.namingServer.NamingServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Client {
+    public static void main(String[] args) {
+        SpringApplication.run(NamingServer.class, args);
+    }
 }
