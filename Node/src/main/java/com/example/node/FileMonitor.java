31f28ea0050039f3c5969765bf10a523a1315ee7
Simply Done <abcdeffect1212@gmail.com>
2024-05-05 18:54:51 +0200

    Latest save

--- Node/src/main/java/com/example/node/FileMonitor.java
+++ Node/src/main/java/com/example/node/FileMonitor.java
@@ -38,20 +38,17 @@ public class FileMonitor implements Runnable {
             public void onFileCreate(File file) {
 
                 // Calculate hash and report to naming server when a new file is created
-                if (client.isSetupCompleted()) {
+                if (client.isSetupCompleted() && client.namingServerIP != null) {
                     String filename = file.getName();
                     System.out.println("File created: " + filename);
-
-                    int hash = client.computeHash(filename);
                     client.reportFilenameToNamingServer(file.getName());
                 }
             }
 
-
             @Override
             public void onFileDelete(File file) {
 
-                if (client.isSetupCompleted()) {
+                if (client.isSetupCompleted() && client.namingServerIP != null) {
                     // Handle file deletion event if needed
                     System.out.println("File deleted: " + file.getName());
                 }
