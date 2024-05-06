96c7acba9525196bb08edc412c7c43d6199eecfc
Simply Done <abcdeffect1212@gmail.com>
2024-05-06 16:53:45 +0200

    Namingserver updated.

--- NamingServer/src/main/java/com/example/namingserver/controllers/RestControllerNS.java
+++ NamingServer/src/main/java/com/example/namingserver/controllers/RestControllerNS.java
@@ -91,7 +91,7 @@ public class RestControllerNS {
         if (!filename.endsWith(".swp")) {
             String ipAddressString = (String) requestBody.get("ip");
             Inet4Address originalIP = (Inet4Address) InetAddress.getByName(ipAddressString);
-            int operation = Integer.parseInt(requestBody.get("nodeID").toString());
+            int operation = (Integer) requestBody.get("nodeID");
             namingServer.reportLogger(filename, originalIP, operation);
 
         }
