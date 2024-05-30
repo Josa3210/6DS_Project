cee259e73d1c401629616fe15a1ac1010dae31c3
Simply Done <abcdeffect1212@gmail.com>
2024-05-30 11:24:25 +0200

    Last save

--- Node/src/main/java/com/example/node/FileMonitor.java
+++ Node/src/main/java/com/example/node/FileMonitor.java
@@ -70,9 +70,14 @@ public class FileMonitor implements Runnable {
                     // We remove the file from the logger
                     Logger logger = client.getLogger();
                     logger.load();
-                    // int hash = client.computeHash(filename);
-                    // logger.remove(hash); // We remove the hash from the logger.
-                    client.reportFilenameToNamingServer(file.getName(), file.getPath(), 2); // Operation 2 --> file DELETE on replicated node
+                    int hash = client.computeHash(filename);
+                    logger.remove(hash); // We remove the hash from the logger.
+
+                    if(logger.get(hash) == client.currentIP) // we check if the original IP of the file = current IP
+
+                        // if this is the case, the current IP is the IP where the file got downloaded, so we need to make
+                        // sure that the naming server gets noted about this so it can remove the replicated nodes too.
+                        client.reportFilenameToNamingServer(file.getName(), file.getPath(), 2); // Operation 2 --> file DELETE on replicated node
 
                     try {
                         Files.delete(Path.of(filepath));
