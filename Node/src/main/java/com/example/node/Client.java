ba853a6e45afed2c92373e8adeb61cfaa1c9fe06
Simply Done <abcdeffect1212@gmail.com>
2024-06-04 17:25:27 +0200

    Last save

--- Node/src/main/java/com/example/node/Client.java
+++ Node/src/main/java/com/example/node/Client.java
@@ -528,7 +528,7 @@ public class Client implements I_Client {
         public void memberAdded(MembershipEvent membershipEvent) {
             if (startFileMonitor){
                 try {
-                    TimeUnit.SECONDS.sleep(20);
+                    TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
