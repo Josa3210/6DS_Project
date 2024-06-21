bcba3216b6d1e44bf0e22643bb009c254b1982c2
J0sa3210 <joeri.winckelmans@outlook.be>
2024-06-21 13:24:19 +0200

    deb

--- Node/src/main/java/com/example/node/controllers/RestControllerDiscAndBoot.java
+++ Node/src/main/java/com/example/node/controllers/RestControllerDiscAndBoot.java
@@ -100,6 +100,7 @@ public class RestControllerDiscAndBoot {
     public String OpenTCPConnection(@RequestBody Map<String, Object> request) throws IOException {
         String response;
         String ip = (String) request.get("replicated ip");
+        System.out.println("* Opening TCP connections");
         try{
             client.clientSocket = new Socket(ip, 5000);
         } catch (ConnectException e){
